e477056bebbe96f58cc8052fbd778cc4cbd7030e
siddharthsinghgithub <siddharth@sugoilabs.com>
2019-09-15 16:22:45 +0530

    Updates

---
+++ precache-manifest.f42c5950e62f4cc026aeb594b595a502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "798e958631156a58cbe1",
+    "url": "/static/css/main.65331238.chunk.css"
+  },
+  {
+    "revision": "798e958631156a58cbe1",
+    "url": "/static/js/main.798e9586.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "8f441c2ea883507cc09e4a89554a8a69",
+    "url": "/index.html"
+  }
+];
